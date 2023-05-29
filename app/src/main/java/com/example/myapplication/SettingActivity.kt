e24bdf94d22095f39f27fc1864c7281429b651a9
KimSH39 <sooooooohyeon5@naver.com>
2023-05-30 02:46:33 +0900

    feat: 소설 퀄리티 위한 파파고 API 추가

--- app/src/main/java/com/example/myapplication/SettingActivity.kt
+++ app/src/main/java/com/example/myapplication/SettingActivity.kt
@@ -76,27 +76,129 @@ class SettingActivity : AppCompatActivity() {
             val writesumText = binding.writesum.text.toString()
 
             if (writesumText.length > 200) {
-                Toast.makeText(this@SettingActivity, "글자 수를 200자 이내로 제한해주세요.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@SettingActivity, "글자 수를 200자 이내로 제한해 주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            val intent: Intent = Intent(this@SettingActivity, LoadingActivity::class.java)
-            startActivity(intent)
+            var customwritesumText: String
 
-            runGPT3(selectedGenre, selectedEra, numMan, numWoman, writesumText) { responseBody ->
-                val intent = Intent(this@SettingActivity, SubActivity::class.java).apply {
-                    putExtra("next", "level")
-                    putExtra("selectedGenre", selectedGenre)
-                    putExtra("selectedEra", selectedEra)
-                    putExtra("NumMan", numMan)
-                    putExtra("NumWoman", numWoman)
-                    putExtra("num", 30)
-                    putExtra("key", writesumText)
-                    putExtra("summary", responseBody)
-                }
+            translateToEnglish(writesumText) { translatedText ->
+                customwritesumText = translatedText
+
+                val intent: Intent = Intent(this@SettingActivity, LoadingActivity::class.java)
                 startActivity(intent)
+
+                val customGenre = when (selectedGenre) { // 장르 영어로 변경
+                    "로맨스" -> "Romance"
+                    "판타지" -> "Fantasy"
+                    "공상 과학" -> "science fiction"
+                    "스포츠" -> "sports"
+                    else -> "Feel free to fill it out" // 선택한 장르에 해당하지 않는 경우의 기본값 설정
+                }
+
+                val customEra = when (selectedEra) { // 시대 영어로 변경
+                    "현대" -> "Modern "
+                    "미래" -> "Future"
+                    "19세기" -> "19th century"
+                    "르네상스" -> "the Renaissance"
+                    else -> "Feel free to fill it out" // 선택한 시대에 해당하지 않는 경우의 기본값 설정
+                }
+
+                runGPT3(customGenre, customEra, numMan, numWoman, customwritesumText) { responseBody ->
+                    val originalResponseBody = responseBody
+                    translateToKorean(responseBody) { translatedResponseBody ->
+                        Log.d("Response Body:", responseBody)
+                        Log.d("TranslatedRespons:", translatedResponseBody)
+                        val intent = Intent(this@SettingActivity, SubActivity::class.java).apply {
+                            putExtra("next", "level")
+                            putExtra("selectedGenre", customGenre)
+                            putExtra("selectedEra", customEra)
+                            putExtra("NumMan", numMan)
+                            putExtra("NumWoman", numWoman)
+                            putExtra("num", 30)
+                            putExtra("key", customwritesumText)
+                            putExtra("summary", translatedResponseBody)
+                            putExtra("originalsummary", originalResponseBody) // 번역 전의 텍스트 인텐트에 추가
+                        }
+                        startActivity(intent)
+                    }
+                }
             }
         }
+    }
+
+    private fun translateToEnglish(inputText: String, callback: (String) -> Unit) {
+        val client = OkHttpClient.Builder().build()
+        val url = "https://openapi.naver.com/v1/papago/n2mt"
+
+        val requestBody = FormBody.Builder()
+            .add("source", "ko")
+            .add("target", "en")
+            .add("text", inputText)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .addHeader("X-Naver-Client-Id", "Client_ID")
+            .addHeader("X-Naver-Client-Secret", "Client_Secret")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Translation request failed
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                if (body != null) {
+                    val jsonObject = JSONObject(body)
+                    val translatedText = jsonObject.getJSONObject("message")
+                        .getJSONObject("result")
+                        .getString("translatedText")
+
+                    callback(translatedText)
+                }
+            }
+        })
+    }
+
+    private fun translateToKorean(inputText: String, callback: (String) -> Unit) {
+        val client = OkHttpClient.Builder().build()
+        val url = "https://openapi.naver.com/v1/papago/n2mt"
+
+        val requestBody = FormBody.Builder()
+            .add("source", "en")
+            .add("target", "ko")
+            .add("text", inputText)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .addHeader("X-Naver-Client-Id", "Client_ID")
+            .addHeader("X-Naver-Client-Secret", "Client_Secret")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Translation request failed
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                if (body != null) {
+                    val jsonObject = JSONObject(body)
+                    val translatedText = jsonObject.getJSONObject("message")
+                        .getJSONObject("result")
+                        .getString("translatedText")
+
+                    callback(translatedText)
+                }
+            }
+        })
     }
 
     private fun updateOutputText(textView: TextView, value: Int) {
@@ -104,13 +206,14 @@ class SettingActivity : AppCompatActivity() {
     }
 
     private fun runGPT3(
-        selectedGenre: String,
-        selectedEra: String,
+        customGenre: String,
+        customEra: String,
         numMan: Int,
         numWoman: Int,
-        writesumText: String,
+        customwritesumText: String,
         callback: (String) -> Unit
     ) {
+
         val client = OkHttpClient.Builder()
             .connectTimeout(60, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
@@ -121,18 +224,16 @@ class SettingActivity : AppCompatActivity() {
         val url = "https://api.openai.com/v1/chat/completions"
 
         val requestBody = """
-            {
-                "model": "gpt-3.5-turbo",
-                "messages": [
-                    {"role": "user", "content": "The following are the default rules. Keep this rule no matter what."},
-                    {"role": "user", "content": "Write a novel that contains the ending and is probable."},
-                    {"role": "system", "content": "1. All results are printed in Korean."},
-                    {"role": "system", "content": "2. Limit novels to a maximum of 10 sentences."},
-                    {"role": "system", "content": "3. Novel creation time is up to 50 seconds."},
-                    {"role": "system", "content": "4. Every novel's sentence should be clearly written, and the story should be smooth without the wrong words."},
-                    {"role": "user", "content": "등장인물: 남자 `$numMan`명, 여자 `$numWoman`명, 시대: $selectedEra, 장르: $selectedGenre, 줄거리: `$writesumText`로 작성해 줘."}
-                ]
-            }
+        {
+         "model": "gpt-3.5-turbo",
+            "messages": [
+                {"role": "system", "content": "The following are the modified rules for creating a logical and coherent novel."},
+                {"role": "system", "content": "write the title. form is title: title"},
+                {"role": "system", "content": "Limit novels to a maximum of 10 sentences. The sentence should be short and simple."},
+                {"role": "system", "content": "Every novel's sentence should be clearly written, and the story should be smooth without any illogical or inconsistent elements."},
+                {"role": "user", "content": "the number of male characters: $numMan, the number of female characters: $numWoman, the novel's background period: $customEra, a novel genre of fiction: $customGenre, a rough novel or keywords $customwritesumText"}
+            ]
+        }
         """.trimIndent()
 
         val request = Request.Builder()
@@ -150,7 +251,6 @@ class SettingActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string()
                 if (body != null) {
-                    Log.d("API Response Body", body)
                     val jsonObject = JSONObject(body)
                     val jsonArray = jsonObject.getJSONArray("choices")
                     val content = jsonArray.getJSONObject(0).getJSONObject("message").getString("content")
