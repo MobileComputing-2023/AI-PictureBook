f56f7acb402063045453db97133d418dc99e18d2
choijihyeon <gus1043@gmail.com>
2023-05-30 14:30:31 +0900

    feat : Popup 기능 추가

--- app/src/main/java/com/example/myapplication/PopupActivity.kt
+++ app/src/main/java/com/example/myapplication/PopupActivity.kt
@@ -1,9 +1,14 @@
 package com.example.myapplication
 
+import MyDatabase
+import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityPopupBinding
@@ -22,7 +27,19 @@ class PopupActivity : AppCompatActivity() {
     private fun showPopupDialog() {
         // 팝업창 생성
         val builder = AlertDialog.Builder(this)
-        builder.setView(binding.root)
+
+        // LinearLayout을 생성하고 binding의 루트 뷰를 추가합니다.
+        val container = LinearLayout(this)
+        // Remove the view from its current parent
+        val parent = binding.root.parent as? ViewGroup
+        parent?.removeView(binding.root)
+
+        // Disable clicks and focus for the root view
+        binding.root.isClickable = false
+        binding.root.isFocusable = false
+
+        // Add the view to the new parent
+        container.addView(binding.root)
 
         binding.saveBtn.setOnClickListener {
             // 저장 버튼 클릭 시 동작을 구현
@@ -30,17 +47,33 @@ class PopupActivity : AppCompatActivity() {
 
             // 팝업창 닫기
             finish()
+
+            // Toast 메시지 표시
+            Toast.makeText(this, "동화가 생성되었습니다.", Toast.LENGTH_SHORT).show()
+            // 동화리스트 화면으로 가기
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.deleteBtn.setOnClickListener {
-            // 삭제 버튼 클릭 시 동작 구현
-            // TODO: 삭제 버튼 동작 구현
-
+            // Retrieve the instance of MyDatabase
+            val myDatabase = MyDatabase.getInstance(this)
+            // Delete the last created database
+            myDatabase.deleteLastCreatedRow(MyDatabase.MyDBContract.DrawEntry.TABLE_NAME)
             // 팝업창 닫기
+            finish()
+            // Toast 메시지 표시
+            Toast.makeText(this, "동화가 저장되지않았습니다.\n 메인화면으로 돌아갑니다.", Toast.LENGTH_SHORT).show()
+
+            // 메인 화면으로 돌아가기
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
             finish()
         }
         // 팝업창 생성 및 표시
         val alertDialog = builder.create()
+        alertDialog.setView(container) // 이 부분에서 뷰를 설정합니다.
         alertDialog.show()
     }
 }
