2379b7637be84c3bf3d567aa6a22a60c6b21329b
choijihyeon <gus1043@gmail.com>
2023-06-06 16:51:08 +0900

    feat : 이용약관 추가(레이아웃, 미동의시 앱 이용 불가)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private var backPressedTime: Long = 0
     private val backPressedTimeout: Long = 2000
+
     override fun onBackPressed() {
         if (System.currentTimeMillis() - backPressedTime < backPressedTimeout) {
             // 앱 종료
@@ -18,20 +20,38 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        binding.settingBtn.setOnClickListener {
-            val intent: Intent = Intent(this, SettingActivity::class.java)
-            startActivity(intent)
-        }
+        val sharedPreferences = getSharedPreferences("TermsAgree", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPreferences.getBoolean("termsAgreed", false)
 
-        binding.readBookListBtn.setOnClickListener {
-            val intent: Intent = Intent(this, ListActivity::class.java)
-            startActivity(intent)
+        if (!isFirstTime) {
+            // isFirstTime이 true인 경우 동작 실행
+            binding.settingBtn.setOnClickListener {
+                val intent = Intent(this, TermsActivity::class.java)
+                startActivity(intent)
+            }
+
+            binding.readBookListBtn.setOnClickListener {
+                val intent = Intent(this, TermsActivity::class.java)
+                startActivity(intent)
+            }
+
+        } else {
+            binding.settingBtn.setOnClickListener {
+                val intent: Intent = Intent(this, SettingActivity::class.java)
+                startActivity(intent)
+            }
+
+            binding.readBookListBtn.setOnClickListener {
+                val intent: Intent = Intent(this, ListActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
